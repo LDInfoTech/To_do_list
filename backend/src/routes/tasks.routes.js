39cfb0f4ede7c46561e6a1a80db1f8143230ad67
LDInfoTech <ldinfotech.kof@gmail.com>
2025-09-05 12:17:10 -0300

    Rotas
    Correções

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ctrl = require("../controllers/taskController");
+const ctrl = require("../controllers/task.controller");
 const router = express.Router();
 
 router.get("/tasks", ctrl.list);
