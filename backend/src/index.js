4c3404b20101e1cb33198e31ddea53c6b93fc252
LDInfoTech <ldinfotech.kof@gmail.com>
2025-09-05 12:18:55 -0300

    Servidor principal

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,20 @@
-require("dotenv").config(); //carrega .env para process.env
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const port = process.env.PORT || 3001;
+const taskRoutes = require("./routes/tasks.routes");
+const errorHandler = require("./middlewares/errorHandler");
+// um middleware de tratamento de erros
+const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
+// Rotas
+app.use("/tasks", taskRoutes);
+app.use(errorHandler);
+// Inicia o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API rodando em http://localhost:${PORT}`));
